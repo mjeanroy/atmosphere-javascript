467a792a32ed2d89e0f509bbab3cc3eaa379dd62
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2019-08-27 09:45:04 +0200

    chore: add unit test

--- modules/javascript/src/test/webapp/javascript/atmosphere-ws.test.js
+++ modules/javascript/src/test/webapp/javascript/atmosphere-ws.test.js
@@ -34,20 +34,14 @@ describe('atmosphere WS', () => {
   });
 
   it('should subscribe using given URL', () => {
-    const url = 'wss://localhost/ws';
-    const transport = 'websocket';
-    const logLevel = 'debug';
-    const subscription = atmosphere.subscribe({
-      url,
-      transport,
-      logLevel,
-    });
+    const socket = subscribe({});
+    expect(socket).toBeDefined();
 
-    expect(subscription).toBeDefined();
-    expect(subscription.request).toBeDefined();
-    expect(subscription.request.url).toBe(url);
-    expect(subscription.request.logLevel).toBe(logLevel);
-    expect(subscription.request.transport).toBe('websocket');
+    const rq = socket.request;
+    expect(rq).toBeDefined();
+    expect(rq.url).toBe('wss://localhost/ws');
+    expect(rq.logLevel).toBe('debug');
+    expect(rq.transport).toBe('websocket');
 
     const connections = jasmine.ws().connections();
     expect(connections.count()).toBe(1);
@@ -71,38 +65,234 @@ describe('atmosphere WS', () => {
     );
   });
 
+  it('should subscribe to websocket and trigger connect timeout after interval', () => {
+    const connectTimeout = 3000;
+    const onOpen = jasmine.createSpy('onOpen');
+    const onMessage = jasmine.createSpy('onMessage');
+    const onClose = jasmine.createSpy('onClose').and.callFake((e) => {
+      expect(e.status).toBe(501);
+      expect(e.responseBody).toBe('');
+      expect(e.reason).not.toBeDefined();
+      expect(e.transport).toBe('websocket');
+      expect(e.headers).toEqual([]);
+    });
+
+    subscribe({
+      connectTimeout: 3000,
+      onOpen,
+      onMessage,
+      onClose,
+    });
+
+    expect(onOpen).not.toHaveBeenCalled();
+    expect(onMessage).not.toHaveBeenCalled();
+    expect(onClose).not.toHaveBeenCalled();
+
+
+    jasmine.clock().tick(connectTimeout + 1);
+
+    expect(onOpen).not.toHaveBeenCalled();
+    expect(onMessage).not.toHaveBeenCalled();
+    expect(onClose).toHaveBeenCalled();
+
+    const tt = new Date(now.getTime() + connectTimeout + 1).toString();
+
+    expect(console.debug).toHaveBeenCalledWith(`${tt} Atmosphere: websocket.onclose`);
+    expect(console.debug).toHaveBeenCalledWith(`${tt} Atmosphere: Firing onClose (closed case)`);
+    expect(console.debug).toHaveBeenCalledWith(`${tt} Atmosphere: Request already closed, not firing onClose (closed case)`);
+    expect(console.debug).toHaveBeenCalledWith(`${tt} Atmosphere: Websocket failed on first connection attempt. Downgrading to streaming and resending`);
+    expect(console.warn).toHaveBeenCalledWith(`${tt} Atmosphere: Websocket closed, reason: The endpoint is terminating the connection due to a protocol error. - wasClean: false`);
+
+    expect(console.info).not.toHaveBeenCalled();
+    expect(console.error).not.toHaveBeenCalled();
+  });
+
+  it('should subscribe to websocket and do not trigger connect timeout after interval if connect timeout is not set', () => {
+    const onOpen = jasmine.createSpy('onOpen');
+    const onMessage = jasmine.createSpy('onMessage');
+    const onClose = jasmine.createSpy('onClose');
+
+    subscribe({
+      onOpen,
+      onMessage,
+      onClose,
+    });
+
+    expect(onOpen).not.toHaveBeenCalled();
+    expect(onMessage).not.toHaveBeenCalled();
+    expect(onClose).not.toHaveBeenCalled();
+
+    tick(10000);
+
+    expect(onOpen).not.toHaveBeenCalled();
+    expect(onMessage).not.toHaveBeenCalled();
+    expect(onClose).not.toHaveBeenCalled();
+
+    expect(console.debug).toHaveBeenCalledOnce();
+    expect(console.info).not.toHaveBeenCalled();
+    expect(console.warn).not.toHaveBeenCalled();
+    expect(console.error).not.toHaveBeenCalled();
+  });
+
   describe('once subscribed', () => {
     let connection;
     let onOpen;
     let onMessage;
+    let onClose;
 
     beforeEach(() => {
       onOpen = jasmine.createSpy('onOpen');
       onMessage = jasmine.createSpy('onMessage');
+      onClose = jasmine.createSpy('onClose');
 
-      atmosphere.subscribe({
-        url: 'wss://localhost/ws',
-        transport: 'websocket',
-        logLevel: 'debug',
+      subscribe({
         onOpen,
         onMessage,
+        onClose,
       });
 
       connection = jasmine.ws().connections().mostRecent();
+
+      resetGlobalSpies();
     });
 
     it('should trigger on open once opened', () => {
       connection.openHandshake().respond();
+
       expect(connection.readyState).toBe(1);
       expect(onOpen).not.toHaveBeenCalled();
       expect(onMessage).not.toHaveBeenCalled();
 
+      onOpen.and.callFake((e) => {
+        expect(e.status).toBe(200);
+        expect(e.responseBody).toBe('');
+        expect(e.reason).not.toBeDefined();
+        expect(e.transport).toBe('websocket');
+        expect(e.headers).toEqual([]);
+      });
+
+      onMessage.and.callFake((e) => {
+        expect(e.status).toBe(200);
+        expect(e.responseBody).toBe('X|');
+        expect(e.reason).not.toBeDefined();
+        expect(e.transport).toBe('websocket');
+        expect(e.headers).toEqual([]);
+      });
+
       // Since Atmosphere protocol is enabled, the first message will trigger the `onOpen` callback
       connection.emitMessage('45|729e6d1c-9235-4c42-b889-b178ac4be424|10000|X|');
 
-      expect(onOpen).toHaveBeenCalledWith(jasmine.objectContaining({
-        status: 200,
-      }));
+      expect(onOpen).toHaveBeenCalledOnce();
+      expect(onMessage).toHaveBeenCalledOnce();
+
+      expect(console.info).not.toHaveBeenCalled();
+      expect(console.warn).not.toHaveBeenCalled();
+      expect(console.error).not.toHaveBeenCalled();
+
+      expect(console.debug).toHaveBeenCalledWith(`${now.toString()} Atmosphere: websocket.onopen`);
+      expect(console.debug).toHaveBeenCalledWith(`${now.toString()} Atmosphere: Websocket successfully opened`);
+      expect(console.debug).toHaveBeenCalledWith(`${now.toString()} Atmosphere: websocket.onmessage`);
+      expect(console.debug).toHaveBeenCalledWith(`${now.toString()} Atmosphere: Firing onOpen`);
+      expect(console.debug).toHaveBeenCalledWith(`${now.toString()} Atmosphere: Firing onMessage`);
     });
   });
+
+  describe('once opened', () => {
+    let connection;
+    let onOpen;
+    let onMessage;
+    let onClose;
+
+    beforeEach(() => {
+      onOpen = jasmine.createSpy('onOpen');
+      onMessage = jasmine.createSpy('onMessage');
+      onClose = jasmine.createSpy('onClose');
+
+      subscribe({
+        onOpen,
+        onMessage,
+        onClose,
+      });
+
+      connection = jasmine.ws().connections().mostRecent();
+      connection.openHandshake().respond();
+      connection.emitMessage('45|729e6d1c-9235-4c42-b889-b178ac4be424|10000|X|');
+
+      tick(1000);
+      resetGlobalSpies();
+      resetSpies(onOpen, onClose, onMessage);
+    });
+
+    it('should receive message', () => {
+      const message = 'Hello John Doe';
+
+      onMessage.and.callFake((e) => {
+        expect(e.responseBody).toBe(message);
+
+        expect(e.status).toBe(200);
+        expect(e.reason).not.toBeDefined();
+        expect(e.transport).toBe('websocket');
+        expect(e.headers).toEqual([]);
+      });
+
+      connection.emitMessage(message);
+
+      expect(onOpen).not.toHaveBeenCalled();
+      expect(onClose).not.toHaveBeenCalled();
+      expect(onMessage).toHaveBeenCalledOnce();
+    });
+  });
+
+  /**
+   * Tick clock.
+   *
+   * @param {number} delay Delay, default is zero.
+   * @return {void}
+   */
+  function tick(delay = 0) {
+    jasmine.clock().tick(delay);
+  }
+
+  /**
+   * Subscribe to WebSocket.
+   *
+   * @param {Object} param0 Options.
+   * @return {Object} The opened socket.
+   */
+  function subscribe({onOpen, onClose, onMessage, connectTimeout}) {
+    return atmosphere.subscribe({
+      url: 'wss://localhost/ws',
+      transport: 'websocket',
+      logLevel: 'debug',
+      connectTimeout,
+
+      onOpen,
+      onMessage,
+      onClose,
+    });
+  }
+
+  /**
+   * Reset all global spies.
+   *
+   * @return {void}
+   */
+  function resetGlobalSpies() {
+    console.debug.calls.reset();
+    console.info.calls.reset();
+    console.warn.calls.reset();
+    console.error.calls.reset();
+  }
+
+  /**
+   * Reset all given spies.
+   *
+   * @param  {...any} spies Spies to reset.
+   * @return {void}
+   */
+  function resetSpies(...spies) {
+    for (let i = 0, size = spies.length; i < size; ++i) {
+      spies[i].calls.reset();
+    }
+  }
 });
